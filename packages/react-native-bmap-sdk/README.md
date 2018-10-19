7bbdf78d1faf547bb917bec51393ea2c17abff5c
谢杨 <625233046@qq.com>
2018-10-19 11:38:10 +0800

    百度地图

---
+++ packages/react-native-bmap-sdk/README.md
@@ -0,0 +1,9 @@
+# React Native 百度地图
+> fork from [qiuxiang/react-native-baidumap-sdk](https://github.com/qiuxiang/react-native-baidumap-sdk). 调整了目录结构
+
+### 安装步骤
+- `yarn add react-native-bmap-sdk`
+- `react-native link react-native-bmap-sdk`
+
+### 使用
+> 参见 docs 目录
---
+++ packages/react-native-bmap-sdk/index.js
@@ -0,0 +1,18 @@
+import MapView from './src/map-view'
+import Location from './src/modules/location'
+import Geocode from './src/modules/geocode'
+import Initializer from './src/modules/initializer'
+
+export default {
+  MapView,
+  Location,
+  Geocode,
+  Initializer,
+}
+
+export {
+  MapView,
+  Location,
+  Geocode,
+  Initializer,
+}
